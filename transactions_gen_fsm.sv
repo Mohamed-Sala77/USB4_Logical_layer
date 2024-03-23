532bbc79c5a7dbe2253e92a1f2b85f8d29b72977
ahmedzakaria639 <162883825+ahmedzakaria639@users.noreply.github.com>
2024-03-23 20:23:59 +0200

    Update transactions_gen_fsm.sv

--- transactions_gen_fsm.sv
+++ transactions_gen_fsm.sv
@@ -26,13 +26,14 @@ module transactions_gen_fsm (
 	input            rst,                              //global reset 
 
 	input [ 23 : 0 ] sb_read,                         //data read from the SB register in case of AT read response 
-	input [ 7  : 0 ] control_unit_data,              //data Value to write in AT write command or LEN field or address
+	//input [ 7  : 0 ] control_unit_data,              //data Value to write in AT write command or LEN field or address
 	input [ 2  : 0 ] trans_sel,                     //transaction select wire
 	input            disconnect_sbtx,  
 	input            tdisconnect_tx_min,  
 
 
-	output reg [ 9 : 0 ] trans,                   //output including start and stop bit
+	output reg [ 9 : 0 ] trans,                  //output including start and stop bit
+	output reg [ 1 : 0 ] trans_state,            //disconnected, IDLE or started a transaction
 	output reg           crc_en,                 //CRC enable data for the pauload only not the delimiters 
 	output reg           sbtx_sel,               //mux select signal to show when the data is finished to add CRC parity bits
 	output reg           trans_sent,             //indicates transaction sent
@@ -67,10 +68,11 @@ DATA_READ_RESPONSE_ADDRESS = 'b01111,
 DATA_READ_RESPONSE_LENGTH = 'b10000,
 DATA_READ_RESPONSE_DATA = 'b10001,
 
+CRC1='b10010,
+CRC2='b10011,
 
-
-DLE2 = 'b10010,
-ETX = 'b10011
+DLE2 = 'b10100,
+ETX = 'b10101
 
 } state;
 
@@ -90,7 +92,9 @@ localparam STX_RESPONSE_SYMBOL = 8'b00000100;
 localparam LSE_SYMBOL = 8'b10000000;
 localparam CLSE_SYMBOL = ~LSE_SYMBOL;
 
-
+localparam DISCONNECTED_S = 2'h0,
+	       IDLE_S         = 2'h1,
+		   START          = 2'h2;
 
 
 
@@ -101,6 +105,7 @@ reg [3:0] ser_clk_cycles;
 
 //registers to ensure the output of the data at rising edge of the clock
 reg [9:0] trans_reg ;
+reg [1:0] trans_state_reg ;
 reg	crc_en_reg ;
 reg	sbtx_sel_reg;
 
@@ -117,7 +122,7 @@ always @ (posedge sb_clk or negedge rst) begin
 	end else if (trans_sel != 0) begin
 		trans_sel_reg <= trans_sel;
 		trans_sent <= 0;
-	end else if ((cs==CLSE || cs==ETX) && (ser_clk_cycles==7)) begin
+	end else if ((cs==CLSE || cs==ETX) && (ser_clk_cycles==9)) begin
 		trans_sel_reg <= 0;
 		trans_sent <= 1;
 	end else begin
@@ -151,6 +156,7 @@ always @(*) begin
 
 				ns = DISCONNECT;
 				trans_reg = 0;
+				trans_state_reg = DISCONNECTED_S;
 				crc_en_reg = 0;
 				sbtx_sel_reg=0;
 
@@ -158,6 +164,7 @@ always @(*) begin
 
 				ns = IDLE;
 				trans_reg = 10'b1111111111;
+				trans_state_reg = IDLE_S;
 				crc_en_reg = 0;
 				sbtx_sel_reg=0;
 
@@ -170,6 +177,7 @@ always @(*) begin
 				0: begin 
 					ns = IDLE;
 					trans_reg = 10'b1111111111;
+					trans_state_reg = IDLE_S;
 					crc_en_reg = 0;
 					sbtx_sel_reg=0;
 				end 
@@ -177,6 +185,7 @@ always @(*) begin
 				2: begin 
 					ns = DLE1;
 					trans_reg = {1'b1,8'hFE,1'b0};
+					trans_state_reg = START;
 					crc_en_reg = 0;
 					sbtx_sel_reg=0;
 				end 
@@ -184,6 +193,7 @@ always @(*) begin
 				3:begin 
 					ns = DLE1;
 					trans_reg = {1'b1,8'hFE,1'b0};
+					trans_state_reg = START;
 					crc_en_reg = 0;
 					sbtx_sel_reg=0;
 				end 
@@ -191,6 +201,7 @@ always @(*) begin
 				4: begin 
 					ns = DLE1;
 					trans_reg = {1'b1,8'hFE,1'b0};
+					trans_state_reg = START;
 					crc_en_reg = 0;
 					sbtx_sel_reg=0;
 				end 
@@ -198,6 +209,7 @@ always @(*) begin
 				default: begin 
 					ns = IDLE;
 					trans_reg = 10'b1111111111;
+					trans_state_reg = IDLE;
 					crc_en_reg = 0;
 					sbtx_sel_reg=0;
 				end
@@ -208,27 +220,27 @@ always @(*) begin
 
 		DLE1: begin 
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg)
 
 					2: begin 
 						ns = STX_COMMAND;
 						trans_reg = {1'b1,STX_COMMAND_SYMBOL,1'b0};
-						crc_en_reg = 1;
+						crc_en_reg = 0;
 						sbtx_sel_reg=0;
 					end 
 
 					3:begin 
 						ns = STX_RESPONSE;
 						trans_reg = {1'b1,STX_RESPONSE_SYMBOL,1'b0};
-						crc_en_reg = 1;
+						crc_en_reg = 0;
 						sbtx_sel_reg=0;
 					end 
 
 					4: begin 
 						ns = LSE;
 						trans_reg = {1'b1,LSE_SYMBOL,1'b0};
-						crc_en_reg = 1;
+						crc_en_reg = 0;
 						sbtx_sel_reg=0;
 					end 
 
@@ -240,7 +252,9 @@ always @(*) begin
 
 		LSE: begin 
 
-			if (ser_clk_cycles == 7) begin
+			crc_en_reg = 1;
+			
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg)
 
 					4: begin 
@@ -258,14 +272,14 @@ always @(*) begin
 
 		CLSE: begin 
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg)
 
 					4: begin 
 						ns = IDLE;
 						trans_reg = 10'b1111111111;
 						crc_en_reg = 0;
-						sbtx_sel_reg=1;
+						sbtx_sel_reg=0;
 					end 
 
 				endcase
@@ -274,10 +288,11 @@ always @(*) begin
 			
 		end
 
-
 		STX_COMMAND: begin 
 
-			if (ser_clk_cycles == 7) begin
+			crc_en_reg = 1;
+			
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg)
 
 /*
@@ -291,7 +306,7 @@ always @(*) begin
 
 				2: begin 
 					ns = DATA_READ_COMMAND_ADDRESS;
-					trans_reg = {1'b1,control_unit_data,1'b0};
+					trans_reg = {1'b1,8'd78,1'b0};
 					crc_en_reg = 1;
 					sbtx_sel_reg=0;
 				end 
@@ -360,12 +375,12 @@ always @(*) begin
 
 		DATA_READ_COMMAND_ADDRESS: begin 
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg) 
 
 					2: begin 
 						ns = DATA_READ_COMMAND_LENGTH;
-						trans_reg = {1'b1,8'h24,1'b0};
+						trans_reg = {1'b1,7'h2,1'b0,1'b0};
 						crc_en_reg = 1;
 						sbtx_sel_reg=0;
 					end 
@@ -379,13 +394,13 @@ always @(*) begin
 
 		DATA_READ_COMMAND_LENGTH: begin 
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg) 
 
 					2: begin 
-						ns = DLE2;
-						trans_reg = {1'b1,8'hFE,1'b0};
-						crc_en_reg = 0;
+						ns = CRC1;
+						trans_reg = 0;
+						crc_en_reg = 1;
 						sbtx_sel_reg=0;
 					end 
 
@@ -401,7 +416,9 @@ always @(*) begin
 
 		STX_RESPONSE: begin 
 
-			if (ser_clk_cycles == 7) begin
+			crc_en_reg = 1;
+			
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg)
 
 /*
@@ -415,7 +432,7 @@ always @(*) begin
 
 				3: begin 
 					ns = DATA_READ_RESPONSE_ADDRESS;
-					trans_reg = {1'b1,control_unit_data,1'b0};
+					trans_reg = {1'b1,8'd78,1'b0};
 					crc_en_reg = 1;
 					sbtx_sel_reg=0;
 				end 
@@ -475,12 +492,12 @@ always @(*) begin
 
 		DATA_READ_RESPONSE_ADDRESS: begin 
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg) 
 
 					3: begin 
 						ns = DATA_READ_RESPONSE_LENGTH;
-						trans_reg = {1'b1,control_unit_data,1'b0};
+						trans_reg = {1'b1,7'h5,1'b0,1'b0};
 						crc_en_reg = 1;
 						sbtx_sel_reg=0;
 					end 
@@ -494,12 +511,12 @@ always @(*) begin
 
 		DATA_READ_RESPONSE_LENGTH: begin 
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg) 
 
 					3: begin 
 						ns = DATA_READ_RESPONSE_DATA;
-						trans_reg = {1'b1,8'd3,1'b0};
+						trans_reg = {1'b1,sb_read[23:16],1'b0};
 						crc_en_reg = 1;
 						sbtx_sel_reg=0;
 					end 
@@ -514,39 +531,30 @@ always @(*) begin
 
 		DATA_READ_RESPONSE_DATA: begin
 
-			if (ser_clk_cycles == 7) begin
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg) 
 
 					3: begin 
 						if (data_clock_cycles==2) begin
-							ns = DLE2;
-							trans_reg = {1'b1,8'hFE,1'b0};
-							crc_en_reg = 0;
+							ns = CRC1;
+							trans_reg = 0;
+							crc_en_reg = 1;
 							sbtx_sel_reg=0;
 						end else begin
 							case (data_clock_cycles)
 
 								0: begin 
 									ns = DATA_READ_RESPONSE_DATA;
-									trans_reg = {1'b1,sb_read[7:0],1'b0};
+									trans_reg = {1'b1,sb_read[15:8],1'b0};
 									crc_en_reg = 1;
 									sbtx_sel_reg=0;
 								end
 
 								1: begin
 									ns = DATA_READ_RESPONSE_DATA;
-									trans_reg = {1'b1,sb_read[15:8],1'b0};
+									trans_reg = {1'b1,sb_read[7:0],1'b0};
 									crc_en_reg = 1;
 									sbtx_sel_reg=0; 
-								end
-
-
-								2:begin 
-									ns = DATA_READ_RESPONSE_DATA;
-									trans_reg = {1'b1,sb_read[23:16],1'b0};
-									crc_en_reg = 1;
-									sbtx_sel_reg=0;
-
 								end
 
 
@@ -557,6 +565,8 @@ always @(*) begin
 				endcase
 			end 
 
+
+
 			
 
 		end
@@ -564,7 +574,10 @@ always @(*) begin
 
 		DLE2:  begin 
 
-			if (ser_clk_cycles == 7) begin
+			crc_en_reg = 0;
+			sbtx_sel_reg = 0;
+			
+			if (ser_clk_cycles == 9) begin
 				case (trans_sel_reg)
 
 					2: begin 
@@ -609,43 +622,71 @@ always @(*) begin
 
 	ETX:  begin 
 
-		if (ser_clk_cycles == 7) begin
+		if (ser_clk_cycles == 9) begin
 			case (trans_sel_reg)
 
 				2: begin 
 					ns = IDLE;
 					trans_reg = 10'b1111111111;
 					crc_en_reg = 0;
-					sbtx_sel_reg=1;
+					sbtx_sel_reg=0;
 				end 
 
 				3:begin 
 					ns = IDLE;
 					trans_reg = 10'b1111111111;
 					crc_en_reg = 0;
-					sbtx_sel_reg=1;
+					sbtx_sel_reg=0;
 				end 
 /*
 				7: begin 
 					ns = IDLE;
 					trans_reg = 10'b1111111111;
 					crc_en_reg = 0;
-					sbtx_sel_reg=1;
+					sbtx_sel_reg=0;
 				end 
 
 				8:begin 
 					ns = IDLE;
 					trans_reg = 10'b1111111111;
 					crc_en_reg = 0;
-					sbtx_sel_reg=1;
+					sbtx_sel_reg=0;
 				end 
 				*/
 
 			endcase
 		end
-
-
 	end
+
+
+		CRC1: begin 
+
+			sbtx_sel_reg=1;
+			
+			if (ser_clk_cycles == 9) begin
+
+				ns = CRC2;
+				trans_reg = 0;
+				crc_en_reg = 1;
+				sbtx_sel_reg=1;
+
+			end
+			
+		end
+
+		CRC2: begin 
+			  
+			if (ser_clk_cycles == 9) begin
+
+				ns = DLE2;
+				trans_reg = {1'b1,8'hFE,1'b0};
+				crc_en_reg = 1;
+				sbtx_sel_reg = 1;
+
+			end
+
+			
+		end
 
 endcase
 
@@ -662,6 +703,7 @@ always @(posedge sb_clk) begin
 	trans <= trans_reg ;
 	crc_en <= crc_en_reg ;
 	sbtx_sel <= sbtx_sel_reg ;
+	trans_state <= trans_state_reg ;
 	
 	
 end
@@ -672,7 +714,7 @@ end
 always @(posedge sb_clk or negedge rst) begin 
 	if(~rst) begin
 		ser_clk_cycles <= 0;
-	end else if (cs != 0  && cs != 1 && ser_clk_cycles < 7) begin
+	end else if (cs != 0  && cs != 1 && ser_clk_cycles < 9) begin
 		ser_clk_cycles <= ser_clk_cycles + 1;
 	end else begin 
 		ser_clk_cycles <= 0 ;
@@ -687,7 +729,7 @@ end
 always @(posedge sb_clk or negedge rst) begin 
 	if(~rst) begin
 		data_clock_cycles <= 0;
-	end else if (data_clock_cycles != 2 && (cs == DATA_WRITE_COMMAND_DATA|| cs == DATA_READ_RESPONSE_DATA) && ser_clk_cycles == 7) begin
+	end else if (data_clock_cycles != 2 && (cs == DATA_WRITE_COMMAND_DATA|| cs == DATA_READ_RESPONSE_DATA) && ser_clk_cycles == 9) begin
 		data_clock_cycles <= data_clock_cycles + 1;
 	end else if (cs == IDLE) begin 
 		data_clock_cycles <= 0 ;
