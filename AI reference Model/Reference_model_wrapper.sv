42395669b87774e6e03688315c276ebfeff58a44
Karim-Samy1 <karimsamy11@gmail.com>
2024-03-29 16:12:41 +0200

    Config Reference, phase 2 and 4 added to the AI reference model
    - Reference for the Config_space part is now implemented
    - Phase 2 (SBTX high) is now implemented
    - Phase 4 (Training phase) is added but still not complete

--- AI reference Model/Reference_model_wrapper.sv
+++ AI reference Model/Reference_model_wrapper.sv
@@ -1,9 +1,11 @@
 class reference_model_AI;
 
   // Instances of classes
+  /*
   upper_layer_tr upper_layer_tr_inst;
   elec_layer_tr elec_layer_tr_inst;
   config_transaction config_transaction_inst;
+  */
 
   // Mailboxes
   mailbox #(upper_layer_tr) upper_S;
@@ -18,9 +20,13 @@ class reference_model_AI;
   mailbox #(upper_layer_tr) upper_to_elec;
   mailbox #(elec_layer_tr) elec_to_upper;
 
+  mailbox config_to_elec_disable;  
+  mailbox config_to_upper_disable;      
+
   // Instances of upper_ref_AI and elec_ref_AI
   upper_ref_AI upper_ref_AI_inst;
   elec_ref_AI elec_ref_AI_inst;
+  ConfigurationSpacesModel config_ref_AI_inst;
 
   // Constructor
   function new(mailbox #(upper_layer_tr) upper_S, mailbox #(upper_layer_tr) upper_G, mailbox #(elec_layer_tr) elec_S, mailbox #(elec_layer_tr) elec_G, mailbox #(config_transaction) config_S, mailbox #(config_transaction) config_G);
@@ -41,11 +47,18 @@ class reference_model_AI;
     // Create instances of upper_ref_AI and elec_ref_AI
     upper_ref_AI_inst = new(upper_S, upper_G, upper_to_elec, elec_to_upper);
     elec_ref_AI_inst = new(elec_S, elec_G, elec_to_upper, upper_to_elec);
+    config_ref_AI_inst = new(config_G, config_S, config_to_elec_disable, config_to_upper_disable);
+
+    //function new(mailbox #(config_transaction) stimulus_mb, mailbox #(config_transaction) scoreboard_mb, mailbox electrical_mb, mailbox upper_mb);
+
+
+
   endfunction
 
   // Task run
   task run;
     fork
+      config_ref_AI_inst.run();
       upper_ref_AI_inst.run();
       elec_ref_AI_inst.run();
     join
