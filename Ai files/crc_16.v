b32d3e937c033e6c819757b2e8bc157789f1dc8d
SeifFadda <167030038+SeifFadda@users.noreply.github.com>
2024-04-23 17:19:41 +0200

    Update crc_16.v

--- Ai files/crc_16.v
+++ Ai files/crc_16.v
@@ -1,31 +1,31 @@
 `default_nettype none
 
 module crc_16(
-    input wire clk,
-    input wire reset,
-    input wire enable,
-    input wire active,
-    input wire data_in,
-    output reg crc_out
+    input wire sb_clk,
+    input wire rst,
+    input wire crc_en,
+    input wire crc_active,
+    input wire trans_ser,
+    output reg parity
     );
 
 reg [15:0] crc_reg;
 reg [3:0] cnt;
 wire feedback;
 
-assign feedback = crc_reg[15] ^ data_in;
+assign feedback = crc_reg[15] ^ trans_ser;
 
-always @(posedge clk or negedge reset) begin
-    if (!reset) begin
+always @(posedge sb_clk or negedge rst) begin
+    if (!rst) begin
         crc_reg <= 16'hFFFF;
         cnt <= 4'b0;
-    end else if (enable) begin
+    end else if (crc_en) begin
         cnt <= (cnt == 9)? 0 : cnt + 1;
-		if (!active && cnt !=0 && cnt!=9) begin // Normal CRC Calculation Mode
+		if (!crc_active && cnt !=0 && cnt!=9) begin // Normal CRC Calculation Mode
             crc_reg <= {crc_reg[14:0], feedback};
             crc_reg[2] <= crc_reg[1] ^ feedback;
             crc_reg[15] <= crc_reg[14] ^ feedback;
-        end else if (active && cnt!=0 && cnt!=9) begin // Output CRC bits serially
+        end else if (crc_active && cnt!=0 && cnt!=9) begin // Output CRC bits serially
             crc_reg <= {crc_reg[14:0], 1'b0};
         end
     end else begin
@@ -35,14 +35,14 @@ always @(posedge clk or negedge reset) begin
 end
 
 always @(*) begin
-    if (active && cnt==0) begin
-        crc_out = 1'b0;
-    end else if (active && cnt==9) begin
-        crc_out = 1'b1;
-	end else if (active) begin
-	    crc_out = crc_reg[15]; 
+    if (crc_active && cnt==0) begin
+        parity = 1'b0;
+    end else if (crc_active && cnt==9) begin
+        parity = 1'b1;
+	end else if (crc_active) begin
+	    parity = crc_reg[15]; 
 	end else begin
-	    crc_out <= 1'b0;
+	    parity <= 1'b0;
 	end
 end
 
